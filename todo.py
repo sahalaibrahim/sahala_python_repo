754c645709d5926811b9d1a2e1672e1069c0c3fe
sahalaibrahim <sahalaibrahimkoodali@gmail.com>
2024-01-19 11:05:56 +0530

    to do list application with persistance storage

--- todo.py
+++ todo.py
@@ -1,20 +1,34 @@
+import json
 class TodoList:
     toDoItem = []
 
     def addItem(self, name):
         self.toDoItem.append(name)
+        self.writeToJson()
 
     def removeItems(self, index):
         try:
             del self.toDoItem[index]
+            self.writeToJson()
         except:
             print("invalid index")
 
     def viewAllItem(self):
         print("***************Start**************")
-        for item in self.toDoItem:
+        for item in self.readFromJson():
             print(item)
         print("***************End**************")
+    def writeToJson(self):
+        file_path = "example_list.json"
+        with open(file_path, "w") as json_file:
+            json.dump(self.toDoItem, json_file, indent=2)
+            
+    def readFromJson(self):
+        file_path_read = "example_list.json"
+        with open(file_path_read, "r") as json_file:
+            loaded_data = json.load(json_file)
+            self.toDoItem = loaded_data
+            return loaded_data
 
 
 option = 1
